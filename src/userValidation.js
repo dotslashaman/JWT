cce56790efa8961fb5f4920e96984631c8678e34
Aman <meamanx7@gmail.com>
2026-08-06 12:18:28 +0530

    added validations for age,name,email and mobile number

--- src/userValidation.js
+++ src/userValidation.js
@@ -4,18 +4,26 @@ function validateMobileAndEmail(req,res,next){
 
     const userSchema = z.object({
         name : z.string().max(10, {
+
             message : "Name must not be greater than 10 characters"
+
         }),
+
         age : z.number().min(18, {
-            message : "Age must be greater than 18"
+
+            message : "Age must be greater than 18" // checks for age 
+
         }),
-        email : z.string().email(),
+
+        email : z.string().email(), // checks for string mail and validation
+
         mobileNumber : z.string().length(10, {
             message : "Mobile Number Should Be 10 Digits"
         })
     });
 
     const result = userSchema.safeParse(req.body);
+    console.log(result);
     if(!result.success) {
        return res.status(400).json({
         "msg" : "User Validation Failed",
